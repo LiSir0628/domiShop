c0fb94e621bb589ab916f3a2b77e7a01bf19d5c2
mr.lih <769320860@qq.com>
2022-10-15 17:57:47 +0800

    路径配置

---
+++ pages/register/service.vue
@@ -0,0 +1,103 @@
+<template>
+	<view class="container">
+		<view class="text-mask" v-if="isClickService">
+			<view v-html="service_text"></view>
+			<view class="read-btn" @click="read">{{ $t('register.read') }}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				service_text: "",  //服务协议
+				isClickService: false, //是否展示服务协议
+			}
+		},
+		created() {
+			this.lookAgreement()
+		},
+		methods: {
+			lookAgreement() {
+				if(this.service_text){
+					this.isClickService = true
+					return
+				}
+				uni.showLoading({
+					title: this.$t('common').loading + '...',
+					mask: true
+				});
+				this.$myRequest({
+					method: 'GET',
+					url: 'api/general/dict/info',
+					data:{
+						dict_key: "user_service_agreement"
+					}
+				})
+				.then(res=>{
+					if(res.data.code == 200){
+						//console.log(res.data.data.dict_val)
+						this.isClickService = true
+						this.service_text = res.data.data.dict_val
+						uni.hideLoading();
+					} else {
+						uni.hideLoading();
+					}
+				})
+				.catch(err=>{
+					uni.hideLoading();
+					uni.showModal({
+						title: this.$t('common').Tip,
+						content: this.$t('common').Network,
+						confirmText: this.$t('common').confirm,
+						//content: err,
+						showCancel: false,
+					})
+				})
+			},
+			read() {
+				uni.navigateTo({
+					url: "./register"
+				});
+			},
+		}
+	}
+</script>
+
+<style scoped>
+	.container {
+		
+	}
+	/* 协议 */
+	.text-mask{
+		width: 100%;
+		height: 100%;
+		padding: 30rpx 40rpx;
+		box-sizing: border-box;
+		overflow: hidden;
+		overflow-y: auto;
+		position: fixed;
+		top: 0;
+		left: 0;
+		background: #ffffff;
+	}
+	.read-btn {
+		width: 670rpx;
+		height: 80rpx;
+		line-height: 80rpx;
+		background: #FF7436;
+		border-radius: 8rpx;
+	
+		font-size: 24rpx;
+		font-family: Arial;
+		font-weight: 400;
+		color: #FFFFFF;
+		text-align: center;
+		/* position: fixed;
+		bottom: 30rpx;
+		left: 0;
+		right: 0; */
+		margin: 30rpx auto 0;
+	}
+</style>
